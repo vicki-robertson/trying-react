0ab8078a7150607e1a8a031fd0a0836282a18939
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2023-12-26 11:32:49 +0100

    footer finished with links

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,11 +1,21 @@
 import styles from "./Footer.module.css"
 import SocialMedia from "../SocialMedia/SocialMedia";
-
+import Image from "next/image";
 
 export default function Footer() {
   return (
       <footer className={styles.footer}>
-        <SocialMedia className={styles.socialFooter}/>
+        <div className={styles.smallLogo}>
+          <Image
+            src="/logos/logo-cadena.png"
+            alt='Doctor Who cog Logo'
+            height={38}
+            width={38}
+          />
+        </div>
+        <div className={styles.socialFooter}>
+          <SocialMedia />
+        </div>
       </footer>
   );
 }
